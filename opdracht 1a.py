3b33e34493eb771d571f232fe2c6600f373b3a9a
lwissink <l.wissink1998@gmail.com>
2019-02-15 11:40:04 +0100

    AF

--- opdracht 1a.py
+++ opdracht 1a.py
@@ -1,5 +1,6 @@
 import pprint
 
+import pymongo
 from pymongo import MongoClient
 client = MongoClient()
 
@@ -9,12 +10,23 @@ products = db.products
 profiles = db.profiles
 sessions = db.sessions
 
+def nameandprice():
+    for product in products.find():
+        print(product['name'], (product['price']['selling_price'])/100, 'euro')
+        break
 
-pprint.pprint(products.find_one())
+def r_as_first_letter():
+    for product in products.find():
+        if 'R' == product['name'][0]:
+            print(product['name'])
+            break
 
+def averageprice():
+    prijzen = []
+    for product in products.find():
+        prijzen.append(int(product['price']['selling_price']))
+    print(sum(prijzen)/len(prijzen))
 
-for x in products.find({},{"name": "R%"}):
-    print(x)
-
-for x in products.find({},{"price": 1}):
-    print(x)
+nameandprice()
+r_as_first_letter()
+averageprice()
